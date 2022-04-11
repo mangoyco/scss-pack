cc263221dd23e4c196cd87862c78306a7a32473b
mangoyco <wyc810623295@qq.com>
2022-04-11 13:47:55 +0800

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# 单独的样式开发
+
+## scene
+
+在为一些项目，或者某些html文件单独编写的css文件的时候，使用 `scss` + `weboack` 单独打包 `scss`文件
+
+## How to use
+安装依赖：
+ ```bash
+ npm start
+ ```
+
+ 安装依赖：
+ ```bash
+ npm run dev
+ ```
+
+css文件夹中的代码，即为scss文件夹中对应scss文件
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const path = require("path")
+
+module.exports = {
+  entry: {
+    common: "./scss/common.scss",
+    home: "./scss/home.scss"
+  },
+  output: {
+    path: path.resolve(__dirname, 'css')
+  },
+  module: {
+    rules: [
+      // Extracts the compiled CSS from the SASS files defined in the entry
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          "css-loader",
+          'sass-loader'
+          // {
+          //   loader: "css-loader",
+          //   options: {
+          //     importLoaders: 2
+          //   }
+          // },
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new FixStyleOnlyEntriesPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    })
+  ],
+  // optimization: {
+  //   minimizer: [
+  //     // 有时候webpack会默认优化z-index值，设置默认不优化
+  //     new OptimizeCSSAssetsPlugin({
+  //       cssProcessorOptions: {
+  //         safe: true
+  //       }
+  //     })
+  //   ]
+  // },
+};
